34b01ef076e9fc2472e13892d5d46c5ab9e51032
jh214 <sswed147@naver.com>
2024-05-28 12:32:21 +0900

    Initial commit

---
+++ src/main/java/common/commonpjt/CommonPjtApplication.java
@@ -0,0 +1,13 @@
+package common.commonpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonPjtApplication.class, args);
+	}
+
+}

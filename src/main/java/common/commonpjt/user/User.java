2ee03da083c4ab142209ee53bec23e3b52faef9b
jh214 <sswed147@naver.com>
2024-05-29 18:31:16 +0900

    test

--- src/main/java/common/commonpjt/user/User.java
+++ src/main/java/common/commonpjt/user/User.java
@@ -1,4 +1,12 @@
 package common.commonpjt.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue()
 }
